4c0d04c1c779cb79ef1790989ac46faf943f8472
berest915 <berest915@gmail.com>
2021-02-17 22:05:52 +0800

    add passHref and anchor tag for nav-link, nested-className not applying style in styled-components

--- coms/Nav/Nav.js
+++ coms/Nav/Nav.js
@@ -6,8 +6,8 @@ const Nav = () => {
     <Wrapper>
       {/* //! cover image */}
       <li className="li">
-        <Link href="/" className='link'>
-          <ul>all</ul>
+        <Link href="/" className='link' passHref>
+          <a>all</a>
         </Link>
         {/* <Link href="/products/tee" className='link'>
           <ul>tee</ul>
@@ -15,8 +15,8 @@ const Nav = () => {
         <Link href="/products/mug" className='link'>
           <ul>mug</ul>
         </Link> */}
-        <Link href="/about" className='link'>
-          <ul>about</ul>
+        <Link href="/about" className='link' passHref>
+          <a>about</a>
         </Link>
       </li>
     </Wrapper>
